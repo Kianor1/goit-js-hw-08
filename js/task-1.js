dfa16fbb957b672ab8f6c1445c3808a012b9fbcd
B.O. Vdovych <123995120+Kianor1@users.noreply.github.com>
2024-01-22 17:38:06 +0200

    clear

--- js/task-1.js
+++
@@ -1,9 +0,0 @@
-const categoriesList = document.querySelectorAll('#categories .item');
-console.log('Number of categories:', categoriesList.length);
-
-categoriesList.forEach(category => {
-  const categoryName = category.querySelector('h2').textContent;
-  console.log(`Category: ${categoryName}`);
-  const categoryItemsCount = category.querySelectorAll('li').length;
-  console.log(`Elements: ${categoryItemsCount}`);
-});
